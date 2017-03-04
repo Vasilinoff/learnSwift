b3e4bc1fd62542caba94054016493b6bbd079d96
Vasily Ponomarev <vaspondm@gmail.com>
2017-03-04 18:19:55 +0300

    first time commit this

---
+++ InteractiveStory/Page.swift
@@ -0,0 +1,73 @@
+//
+//  Page.swift
+//  InteractiveStory
+//
+//  Created by Vasily on 28.02.17.
+//  Copyright © 2017 Vasily. All rights reserved.
+//
+
+import Foundation
+
+class Page {
+    let story: Story
+    
+    typealias Choise = (title: String, page: Page)
+    
+    var firstChoice: Choise?
+    var secondChoice: Choise?
+    
+    init(story: Story) {
+        self.story = story
+    }
+}
+extension Page {
+    func addChoiceWith(title: String, story: Story) -> Page {
+        let page = Page(story: story)
+        return addChoiceWith(title: title, page: page)
+    }
+    
+    func addChoiceWith(title: String, page: Page) -> Page {
+        
+        switch (firstChoice, secondChoice) {
+        case (.some, .some): return self
+        case (.none, .none), (.none, .some): firstChoice = (title, page)
+        case (.some, .none): secondChoice = (title, page)
+        }
+        
+        return page
+        
+    }
+}
+
+typealias Degree = Double
+func rotate(by angle: Degree) {
+    
+}
+
+struct Adventure {
+    static var story: Page {
+        let returnTrip = Page(story: .returnTrip)
+        let touchdown = returnTrip.addChoiceWith(title: "Stop and Investigate", story: .touchDown)
+        let homeward = returnTrip.addChoiceWith(title: "Continue home to Earth", story: .homeward)
+        let rover = touchdown.addChoiceWith(title: "Explore the Rover", story: .rover)
+        let crate = touchdown.addChoiceWith(title: "Open the Crate", story: .crate)
+        
+        homeward.addChoiceWith(title: "Head back to Mars", page: touchdown)
+        let home = homeward.addChoiceWith(title: "Continue Home to Earth", story: .home)
+        
+        let cave = rover.addChoiceWith(title: "Explore the Coordinates", story: .cave)
+        rover.addChoiceWith(title: "Return to Earth", page: home)
+        
+        cave.addChoiceWith(title: "Continue towards faint light", story: .droid)
+        cave.addChoiceWith(title: "Refill the ship and explore the rover", page: rover)
+        
+        crate.addChoiceWith(title: "Explore the Rover", page: rover)
+        crate.addChoiceWith(title: "Use the key", story: .monster)
+        
+        return returnTrip
+    }
+}
+
+
+
+
